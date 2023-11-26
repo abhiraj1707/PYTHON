4bae6b49280cb71ccd122294dda0b363d86fd2c4
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-11-26 08:23:48 +0530

    Add files via upload

---
+++ 02. Variables and Data Types/Swap_two_Variables.py
@@ -0,0 +1,25 @@
+x = 100
+y = 200
+
+temp = x
+x = y
+y = temp
+
+print(x)
+print(y)
+
+
+x = 5
+y = 6
+x, y = y, x
+print(x)
+print(y)
+
+x, y, z = 100, "geeks", 10.5
+print(x)
+print(y)
+print(z)
+
+x,y = x-5, y+"for"
+print(x)
+print(y)
