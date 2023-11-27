6d724aacded67dfc00831975a3ccfbed6810e0bb
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-11-27 08:16:22 +0530

    Add files via upload

---
+++ 02. Variables and Data Types/Type()_in_Python.py
@@ -0,0 +1,29 @@
+a = 10
+print(type(a))
+
+b = 10.5
+print(type(b))
+
+c = 2 + 4j
+print(type(c))
+
+a = True
+print(type(a))
+
+b = None
+print(type(b))
+
+str = "gfg"
+print(type(str))
+
+l = [20, 40, 60]
+print(type(l))
+
+t = (10, 20, 30)
+print(type(t))
+
+s = {10, 20, 30}
+print(type(s))
+
+d = {10 : "geek", 20 : "for"}
+print(type(d))
