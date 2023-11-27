dbb73554f977603608c2339df796f222eecfc188
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-11-27 08:14:01 +0530

    Add files via upload

---
+++ 02. Variables and Data Types/List_Introduction.py
@@ -0,0 +1,35 @@
+l = [10, 20, 30, 40, 50]
+print(l)
+print(l[3])
+print(l[-1])
+print(l[-2])
+
+l = [10, 20, 30, 40, 50]
+l.append(30)
+print(l)
+l.insert(1, 15)
+print(l)
+print(15 in l)
+print(l.count(30))
+print(l.index(30))
+
+l = [10, 20, 30, 40, 50, 60, 70, 80]
+l.remove(20)
+print(l)
+print(l.pop(1))
+print(l)
+print(l.pop(2))
+print(l)
+del l[1]
+print(l)
+del l[0:2]
+print(l)
+
+l = [10, 40, 20,  50]
+print(max(l))
+print(min(l))
+print(sum(l))
+l.reverse()
+print(l)
+l.sort()
+print(l)
