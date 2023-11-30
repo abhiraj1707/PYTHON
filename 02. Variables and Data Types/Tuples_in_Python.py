fe9791d4eb74c951d1bba9f519d4ee4a6480bfd8
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-11-30 16:00:15 +0530

    Add files via upload

---
+++ 02. Variables and Data Types/Tuples_in_Python.py
@@ -0,0 +1,18 @@
+t = (10, 20, "Geek")
+print(t)
+t =()
+print(type(t))
+print(t)
+t = (10)
+print(type(t))
+t + (10)
+print(type(t))
+
+
+t = 10, 20, 30, 40, 10
+print(t[2])
+print(t[-1])
+print(t[1:3])
+print(len(t))
+print(t.count(10))
+print(t.index(20))
