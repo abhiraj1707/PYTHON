c0032641c4cf3757d701b0606f5125398ddbe36f
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-12-01 16:25:33 +0530

    Add files via upload

---
+++ 03. Input and Output in Python/Print()_in_Python.py
@@ -0,0 +1,13 @@
+#Print() in Python
+print("Hello")
+print("Welcome", "to", "GFG")
+print()
+print("Hope you are enjoying Python")
+
+
+#end and sep in print()
+print("Welcome" , end = " ")
+print("to GFG")
+print("01", "12", "2023", sep = "-")
+print(10,20,30, sep = "+", end = " ")
+print(40, 50)
