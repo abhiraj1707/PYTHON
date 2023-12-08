fd300f661f37d39cc9d1323b1ef92cc39497355c
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-12-08 08:03:46 +0530

    Add files via upload

---
+++ 03. Input and Output in Python/Input()_in_Python.py
@@ -0,0 +1,13 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+print("Welcome " + name)
+print("Your age :  " + age)
+
+
+#Python program for addition of two input numbers
+x = input("Enter First Number : ")
+y = input("Enter Second Number : ")
+x = int(x)
+y = int(y)
+res = x + y
+print("Sum is", res)
