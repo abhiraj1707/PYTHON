4767716c4eb53911e4de88eb3f858f145fab3770
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-12-13 17:52:43 +0530

    Add files via upload

---
+++ 04. Operators/Arithmetic_Operators.py
@@ -0,0 +1,33 @@
+#program 01
+x = 9
+y = 4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
+
+#program 02
+x = -5
+y = 2
+print(x//y)
+x = 5.0
+y = 2
+print(x//y)
+x = 2
+y = -2
+print(x**y)
+
+
+#program 03
+print(5+2*3)
+print(5+3*4**2)
+
+
+#program 04
+print(5+4-2)
+print(2**2**-1)
+print((2**2)**-1)
