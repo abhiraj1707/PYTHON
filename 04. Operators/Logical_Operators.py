80bec732bccaf1fedf521d3c54d2d372a7dc7d61
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-12-14 18:20:24 +0530

    Add files via upload

---
+++ 04. Operators/Logical_Operators.py
@@ -0,0 +1,19 @@
+a = 10
+b = 20
+c = 30
+print(a<b and b<c)
+print(a<b or b>c)
+print(not a>b)
+
+
+s1 = ""
+s2 = s1 or "DefaultStr"
+print(s2)
+
+
+x = 10
+print(x or 20)
+y = 0
+print(y or 30)
+z = 40
+print(z and 50)
