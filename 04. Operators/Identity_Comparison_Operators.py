ad592b8ae7a2c308ae071772634af6824791d81e
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-12-15 18:56:22 +0530

    Add files via upload

---
+++ 04. Operators/Identity_Comparison_Operators.py
@@ -0,0 +1,20 @@
+x = 10
+y = x
+print(x is y)
+print(x is not y)
+
+
+x1 = 10
+x2 = 10
+y1 = 10.5
+y2 = 10.5
+z1 = "geeksforgeeks"
+z2 = "geeksforgeeks"
+print(x1 is x2)
+print(y1 is y2)
+print(z1 is z2)
+
+
+l1 = [10, 20, 30]
+l2 = [10, 20, 30]
+print(l1 is l2)
