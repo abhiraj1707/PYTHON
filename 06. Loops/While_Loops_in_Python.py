637a4826dd806bfae8543358023055618157749f
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2024-01-15 15:26:23 +0530

    Add files via upload

---
+++ 06. Loops/While_Loops_in_Python.py
@@ -0,0 +1,12 @@
+i = 0
+while i < 5:
+    print("Abhiraj Singh")
+    i = i + 1
+
+
+#Print 'Abhiraj Singh' n times
+n = int(input("Enter a number : "))
+i = 0
+while i < n:
+    print("Abhiraj Singh")
+    i = i + 1
