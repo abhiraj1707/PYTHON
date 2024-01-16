e6bfe81775fa4442356878579898c686737fe053
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2024-01-16 17:37:52 +0530

    Add files via upload

---
+++ 06. Loops/range()_in_Python.py
@@ -0,0 +1,39 @@
+r = range(6)
+print(r)
+l = list(r)
+print(l)
+
+
+r = range(5)
+print(type(r))
+
+
+r = range(10, 20)
+print(r)
+l = list(r)
+print(l)
+
+
+r = range(-2, 2)
+l = list(r)
+print(l)
+
+
+r = range(10, 20, 2)
+l = list(r)
+print(l)
+
+
+r = range(10, 20, 3)
+l = list(r)
+print(l)
+
+
+r = range(20, 10, -2)
+l = list(r)
+print(l)
+
+
+r = range(20, 10, -3)
+l = list(r)
+print(l)
