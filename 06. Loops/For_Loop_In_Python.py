3bc6f129de1e50eba14b8ffeef0c5ed31b302652
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2024-01-17 17:54:21 +0530

    Add files via upload

---
+++ 06. Loops/For_Loop_In_Python.py
@@ -0,0 +1,28 @@
+l = [10, 20, 30, 40]
+for x in l :
+    print(x)
+
+
+s = "Abhiraj Singh"
+for x in s :
+    print(x)
+
+
+s = "gfg"
+for x in s :
+    print(x)
+    print(x)
+
+
+for x in range(5):
+    print(x)
+
+
+for x in range(20):
+    if x % 6 == 0:
+        print(x)
+
+
+l = [10, 20, 30, 40]
+for i in range(len(l)):
+    print(i, l[i])
