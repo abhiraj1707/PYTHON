d79d15a45ba670940cd0bf016e082d3d34fdfd38
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2024-01-18 18:50:26 +0530

    Add files via upload

---
+++ 06. Loops/Table_of_A_Number.py
@@ -0,0 +1,29 @@
+#Using While Loop
+n = int(input("Enter the number : "))
+i = 1
+while i <= 10:
+    print(i*n)
+    i = i+1
+
+
+
+n = int(input("Enter the n : "))
+m = int(input("Enter the m : "))
+i = 1
+while i <= m:
+    print(i*n)
+    i = i+1
+
+
+
+#Using For Loop
+n = int(input("Enter the n : "))
+for i in range (1, 11):
+    print(i*n)
+
+
+
+n = int(input("Enter the n : "))
+m = int(input("Enter the m : "))
+for i in range(1, m+1):
+    print(i*n)
