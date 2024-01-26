c0ae5d6002db227fb46cecb0195b012c85cbd363
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2024-01-26 16:06:16 +0530

    Add files via upload

---
+++ 06. Loops/Nested_Loop_in_Python.py
@@ -0,0 +1,33 @@
+for i in range (1, 1 * 10 + 1, 1):
+    print(i, end =" ")
+print()
+for i in range (2, 2 * 10 + 1, 2):
+    print(i, end =" ")
+print()
+for i in range (3, 3 * 10 + 1, 3):
+    print(i, end =" ")
+print()
+
+
+
+for i in range (1, 11):
+    for j in range(i,i * 10 + 1, i):
+        print(j, end =" ")
+    print()
+
+
+
+for i in range(1, 3):
+    j = 1
+    while j < 3:
+        print(i, j)
+        j = j + 1
+    print("GFG")
+
+
+
+ll = [[10, 20, 30], [40, 50, 60], [70, 80]]
+for l in ll:
+    for x in l:
+        print(x, end = " ")
+    print()
